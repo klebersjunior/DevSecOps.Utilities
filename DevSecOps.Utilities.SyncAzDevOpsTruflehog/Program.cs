cd2c6ddfa50db4953bc6c5d53cec702643fa96d6
Kleber Aquino <kleber.junior@b3digitas.com>
2024-06-21 23:30:16 -0300

    aa

---
+++ DevSecOps.Utilities.SyncAzDevOpsTruflehog/Program.cs
@@ -0,0 +1,9 @@
+using DevSecOps.Utilities.SyncAzDevOpsTruflehog;
+
+
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHostedService<Worker>();
+
+var host = builder.Build();
+host.Run();
