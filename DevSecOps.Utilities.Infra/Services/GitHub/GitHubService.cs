12ae559ceaeb3d197956272f23be65476af4bf88
Kleber <klebersjunior@hotmail.com>
2024-09-24 00:41:32 -0300

    remove git log

--- DevSecOps.Utilities.Infra/Services/GitHub/GitHubService.cs
+++ DevSecOps.Utilities.Infra/Services/GitHub/GitHubService.cs
@@ -23,7 +23,6 @@ namespace DevSecOps.Utilities.Infra.Services.GitHub
         {
             Console.WriteLine("ORGNAME :" + orgName);
 
-            Console.WriteLine("GITPASS :" + UtilEnviroment.GitPass());
 
             List<GitHubSearchResponse> results = new List<GitHubSearchResponse>();
 
